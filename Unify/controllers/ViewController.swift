18ecb1752251b045b478b17e607d179961afc40d
Chrishon <chrishon595@yahoo.com>
2017-12-10 23:23:02 -0500

    First Commit

--- Unify/controllers/ViewController.swift
+++ Unify/controllers/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    lazy var openCameraButton: UIButton = {
+        let btn = UIButton(type: .system)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.setTitle("Open Canera", for: .normal)
+        btn.setTitleColor(UIColor.red, for: .normal)
+        btn.clipsToBounds = true
+        btn.layer.cornerRadius = 8
+        btn.addTarget(self, action: #selector(openCamera(_:)), for: .touchUpInside)
+        return btn
+    }()
+    
+    @objc private func openCamera(_ sender: Any) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
